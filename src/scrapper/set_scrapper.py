1f6a1d8f3ec631e397b3617734cf59d185926be4
Lucas <lucas.bergeron@outlook.fr>
2019-11-14 19:23:54 +0100

    Add set urls

--- src/scrapper/set_scrapper.py
+++ src/scrapper/set_scrapper.py
@@ -8,7 +8,19 @@ class SetScrapper(ItemScrapper):
 	def __init__(self, url, language):
 		super().__init__(url, language)
 
+	def get_urls(self):
+		urls = []
+
+		block = self.soup.findAll("div", {"class": "ak-item-list-preview"})[0]
+		links = block.findChildren("a", recursive=False)
+
+		for link in links:
+			urls.append(self.DOMAIN + link['href'])
+
+		return urls
+
 	def scrap(self):
 		data = super().scrap("ak-encyclo-detail-right")
 		data['level'] = int(data['level'])
+		data['items'] = self.get_urls()
 		return data
