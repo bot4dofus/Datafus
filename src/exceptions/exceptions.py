07aa5ae844194f14f9650d99ee7c37d85ccd8e72
Luc Berge <lucas.bergeron@outlook.fr>
2021-01-25 00:22:58 +0100

    Replaced PageNotFoundException by DatafusException

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class PageNotFoundException(Exception):
+class DatafusException(Exception):
     
     def __init__(self, message):
     	super().__init__(message)
