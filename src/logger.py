6dd1dd232ae13205ddb89aa246651876c1025b8b
Luc Berge <lucas.bergeron@outlook.fr>
2021-01-25 00:22:26 +0100

    Fix flush problem and add error method

--- src/logger.py
+++ src/logger.py
@@ -6,12 +6,19 @@ import utils
 FILE = 'warnings.log'
 
 def log(message):
-	print(message)
+	print(message, flush=True)
 
 def warning(message):
-	message = '[' + utils.get_time() + '] ' + message
+	message = '[WARNING][' + utils.get_time() + '] ' + message
 	log(message)
 	file = open(FILE, 'a')
 	file.write(message + '\n')
 	file.close()
-	
+
+def error(e):
+	message = '[ERROR][' + utils.get_time() + '] ' + str(e)
+	log(message)
+	file = open(FILE, 'a')
+	file.write(message + '\n')
+	file.close()
+    
