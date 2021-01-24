e17c81f7ecab76265d44f708b0d88019b2bd9b09
Luc Berge <lucas.bergeron@outlook.fr>
2021-01-25 00:19:58 +0100

    Fix various bug due to website updates

--- src/scrapper/list_scrapper.py
+++ src/scrapper/list_scrapper.py
@@ -6,35 +6,43 @@ from scrapper.base_scrapper import BaseScrapper
 
 class ListScrapper(BaseScrapper):
 
-	SIZE = 96
+    SIZE = 96
 
-	###########
-	# BUILDER #
-	###########
+    ###########
+    # BUILDER #
+    ###########
 
-	def __init__(self, url, nb):
-		super().__init__(url)
-		self.nb = nb
+    def __init__(self, url):
+        super().__init__(url)
 
-	#########
-	# SCRAP #
-	#########
+    #########
+    # SCRAP #
+    #########
 
-	def scrap(self):
+    def find_urls(self, soup):
+        items = soup.findAll("tr", {"class": "ak-bg-odd"}) + soup.findAll("tr", {"class": "ak-bg-even"})
+        return [self.DOMAIN + item.find_all("td" , recursive=False)[1].find_all("a")[0]['href'] for item in items]
+        
+    def find_nb_pages(self, soup):
+        div = soup.find("div", {"class": "ak-list-info"})
+        if(div != None):
+            nb = int(div.find('strong').text)
+            return math.ceil(nb/self.SIZE)
+        else:
+            return 1
 
-		urls = []
-		nb_page = math.ceil(self.nb/self.SIZE)
+    def scrap(self):
 
-		for i in range(1, nb_page+1):
-			logger.log('Scrapping page ' + str(i) + '/' + str(nb_page))
+        urls = []
+        
+        logger.log('Scrapping page 1/?')
+        soup = self.requests(self.url + "?size=" + str(self.SIZE))
+        nb_page = self.find_nb_pages(soup)
+        urls.extend(self.find_urls(soup))
 
-			soup = self.requests(self.url + "?size=" + str(self.SIZE) + "&page=" + str(i))
-			items = soup.findAll("tr", {"class": "ak-bg-odd"}) + soup.findAll("tr", {"class": "ak-bg-even"})
-			
-			for item in items:
-				urls.append(self.DOMAIN + item.find_all("td" , recursive=False)[1].find_all("a")[0]['href'])
+        for i in range(2, nb_page+1):
+            logger.log('Scrapping page ' + str(i) + '/' + str(nb_page))
+            soup = self.requests(self.url + "?size=" + str(self.SIZE) + "&page=" + str(i))
+            urls.extend(self.find_urls(soup))
 
-		if len(urls) != self.nb:
-			raise Exception('Expected ' + str(self.nb) + ' items, got ' + str(len(urls)))
-
-		return urls
+        return urls
--- src/scrapper/monster_scrapper.py
+++ src/scrapper/monster_scrapper.py
@@ -1,45 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re
+import re, logger
 from scrapper.item_scrapper import ItemScrapper
 
 class MonsterScrapper(ItemScrapper):
 
-	###########
-	# BUILDER #
-	###########
+    ###########
+    # BUILDER #
+    ###########
 
-	def __init__(self, url, language):
-		super().__init__(url, language)
+    def __init__(self, url, language):
+        super().__init__(url, language)
 
-	################
-	# NON OPTIONAL #
-	################
+    ################
+    # NON OPTIONAL #
+    ################
 
-	def get_resistances(self):
-		block = self.get_block('resistances')
-		return self.get_array(block)
+    def get_resistances(self):
+        block = self.get_block('resistances')
+        return self.get_array(block)
 
-	############
-	# OPTIONAL #
-	############
+    ############
+    # OPTIONAL #
+    ############
 
-	def get_level(self):
-		level = super().get_level()
-		matching = re.match(r'([0-9]*) .* ([0-9]*)', level)
-		if(matching):
-			return (int(matching.group(1)), int(matching.group(2)))
-		else:
-			return (int(level), int(level))
+    def get_level(self):
+        level = super().get_level()
+        levels = re.findall("\d+", level)
+        if(len(levels) > 1):
+            return (int(levels[0]), int(levels[1]))
+        else:
+            return (int(level), int(level))
 
-	#########
-	# SCRAP #
-	#########
+    #########
+    # SCRAP #
+    #########
 
-	def scrap(self):
-		data = super().scrap()
-		data['level'] = self.get_level()
-		data['characteristics'] = self.get_characteristics()
-		data['resistances'] = self.get_resistances()
-		return data
+    def scrap(self):
+        data = super().scrap()
+        data['level'] = self.get_level()
+        data['characteristics'] = self.get_characteristics()
+        data['resistances'] = self.get_resistances()
+        return data
