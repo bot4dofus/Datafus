c745d8e59b4629e28d1c7be17acedc9c93ea563e
Luc Berge <lucas.bergeron@outlook.fr>
2021-09-28 21:41:52 +0200

    Add commit and push
    Former-commit-id: bbff51ee97693f6c0c6f2bf9e87e35e7a7a34347

--- devscript/copy_d2o_d2i_swf.sh
+++ devscript/copy_d2o_d2i_swf.sh
@@ -7,3 +7,7 @@ rm ../data/DofusInvoker.swf
 cp $DOFUS/data/common/*.d2o ../data/d2o/
 cp $DOFUS/data/i18n/*.d2i ../data/d2i/
 cp $DOFUS/DofusInvoker.swf ../data/DofusInvoker.swf
+
+git add ../data/
+git commit -m "Update files"
+git push
