57db9955a17093dfb05d9198265a12c29f9357ba
Luc Berge <lucas.bergeron@outlook.fr>
2021-10-05 19:28:39 +0200

    Add logs, add inputs
    Former-commit-id: 90444fa1b5129f5b6c3b7b17812360556a011d2a

--- devscript/copy_d2o_d2i_swf.sh
+++ devscript/copy_d2o_d2i_swf.sh
@@ -4,10 +4,20 @@ DOFUS=$((echo $DOFUS) | sed -r 's/[\]+/\//g')
 rm ../data/d2o/*
 rm ../data/d2i/*
 rm ../data/DofusInvoker.swf
+
+echo "Copying .d2o files..."
 cp $DOFUS/data/common/*.d2o ../data/d2o/
+
+echo "Copying .d2i files..."
 cp $DOFUS/data/i18n/*.d2i ../data/d2i/
+
+echo "Copying DofusInvoker.swf file..."
 cp $DOFUS/DofusInvoker.swf ../data/DofusInvoker.swf
 
 git add ../data/
+git status
+read -p "The following files will be commited. Press enter to continue. Press Ctrl+C to abort."
+
 git commit -m "Update files"
 git push
+read -p "Press enter to continue."
