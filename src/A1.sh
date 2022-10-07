0122093d0862a6a089e5c229445962691b67518c
LucBerge <lucas.bergeron@outlook.fr>
2022-10-07 13:35:48 +0200

    Reduce export time to 5min
    Former-commit-id: e4becf666fd0516d1378934e0a493117a66203a8

--- src/A1.sh
+++ src/A1.sh
@@ -2,5 +2,5 @@ rm -rf $2
 wget https://github.com/jindrapetrik/jpexs-decompiler/releases/download/version14.4.0/ffdec_14.4.0.zip
 unzip ffdec_14.4.0.zip -d ffdec/
 rm ffdec_14.4.0.zip
-java -Xmx7168m -Djna.nosys=true -Dsun.java2d.uiScale=1.0 -jar "ffdec/ffdec.jar" -timeout 360 -exportTimeout 21600 -exportFileTimeout 360 -export all $2 $1
+java -Xmx7168m -Djna.nosys=true -Dsun.java2d.uiScale=1.0 -jar "ffdec/ffdec.jar" -timeout 300 -exportTimeout 21600 -exportFileTimeout 300 -export all $2 $1
 rm -rf ffdec/
