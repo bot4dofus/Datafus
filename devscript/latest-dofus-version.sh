78cf917541c62cdb01a1ea52209c8744da1a07ef
Lucas Bergeron <lucas.bergeron@outlook.fr>
2023-01-17 13:31:22 +0100

    Now get full version name
    Former-commit-id: f92c63784420bf5e9aec750c8920c078645d53eb

--- devscript/latest-dofus-version.sh
+++ devscript/latest-dofus-version.sh
@@ -1,11 +1,3 @@
 xml_title=$(curl -s https://www.dofus.com/fr/rss/changelog.xml | grep "title" | grep -v "Changelog" -m 1)
 title=$(echo ${xml_title#*'<title><![CDATA['} | cut -d']' -f 1)
-
-if [[ $fullstring == *"$substr"* ]]; then
-	IFS=' ' read -a version_array <<< $title
-	version=$(echo "${version_array[-1]}")
-else
-	version=title
-fi
-
-echo $version
+echo $title
