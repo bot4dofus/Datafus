058a07436e273b54cd926c7582c739fd789890e4
Lucas Bergeron <lucas.bergeron@outlook.fr>
2023-01-23 23:47:44 +0100

    Add read single id method
    Former-commit-id: 844c4850664e435b19a9189fa3c35b49d0405e38

--- src/C1.py
+++ src/C1.py
@@ -11,6 +11,10 @@ def save_file(file_name, data):
 
 class DofusFileReader():
 
+    INT_SIZE = 4
+    SHORT_SIZE = 2
+    BOOL_SIZE = 1
+
     def __init__(self, file, file_format):
         self.file = file
         self.file_format = file_format
@@ -18,21 +22,33 @@ class DofusFileReader():
     def readBytes(self, f, size):
         return f.read(size)
 
+    def skip(self, f, length):
+        return f.seek(f.tell() + length)
+
     def readInt(self, f):
-        r = struct.unpack('>i', self.readBytes(f, 4))
+        r = struct.unpack('>i', self.readBytes(f, self.INT_SIZE))
         return r[0]
+
+    def skipInt(self, f):
+        self.skip(f, self.INT_SIZE)
 
     def readShort(self, f):
-        r = struct.unpack('>h', self.readBytes(f, 2))
+        r = struct.unpack('>h', self.readBytes(f, self.SHORT_SIZE))
         return r[0]
+
+    def skipShort(self, f):
+        self.skip(f, self.SHORT_SIZE)
 
     def readBool(self, f):
-        r = struct.unpack('>?', self.readBytes(f, 1))
+        r = struct.unpack('>?', self.readBytes(f, self.BOOL_SIZE))
         return r[0]
 
+    def skipBool(self, f):
+        self.skip(f, self.BOOL_SIZE)
+
     def readUtf(self, f):
-        # Security mesure for the utf in location 4139591 which has a negative length... this is an error from dofus devs
         stringLength = self.readShort(f)
+        # Security mesure for the utf in location 4139591 which has a negative length... this is an error from dofus devs
         if(stringLength > 0):
             return self.readBytes(f, stringLength).decode()
         return ""
@@ -52,7 +68,7 @@ class D2IReader(DofusFileReader):
     def __init__(self, file):
         super().__init__(file, self.FORMAT)
 
-    def read(self):
+    def read_all(self):
         data = {}
 
         with open(self.file, "rb") as f:
@@ -71,10 +87,41 @@ class D2IReader(DofusFileReader):
                 data[str(id)] = self.readString(f, self.readInt(f))
 
                 if(diacriticExists):
-                    # Do not read the diacritic utf
-                    self.readInt(f)
+                    # Skip the string
+                    self.skipInt(f)
 
         return data
+
+    def read(self, id_to_search):
+        result = None
+
+        with open(self.file, "rb") as f:
+
+            # Index location
+            indexLocation = self.readInt(f)
+            f.seek(indexLocation)
+
+            # IndexTable
+            indexSize = self.readInt(f)
+            endIndexLocation = f.tell() + indexSize
+
+            while f.tell() < endIndexLocation:
+                id = self.readInt(f)
+                diacriticExists = self.readBool(f)
+
+                if(id_to_search <= id):
+                    if(id_to_search == id):
+                        result = self.readString(f, self.readInt(f))
+                    break
+
+                # Skip the string
+                self.skipInt(f)
+
+                if(diacriticExists):
+                    # Skip the string
+                    self.skipInt(f)
+
+        return result.encode().decode('unicode-escape')
 
 
 def main(input, output, is_files):
@@ -91,7 +138,7 @@ def main(input, output, is_files):
         try:
             print("Extracting " + str(key), flush=True)
             reader = D2IReader(key)
-            data = reader.read()
+            data = reader.read_all()
             print("Building " + str(files_to_convert[key]), flush=True)
             save_file(files_to_convert[key], data)
         except Exception as e:
